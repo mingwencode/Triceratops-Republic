7a45e78dc3fbd7ac8baf791ab95760496563fb3b
Yiming Wen <mingwen314@gmail.com>
2021-03-03 14:23:06 -0800

    related product carosuel is able to display 4 cards at a time and cards are dynamically generated

--- Client/src/component/RelatedProductsList.jsx
+++ Client/src/component/RelatedProductsList.jsx
@@ -7,45 +7,89 @@ const Title = styled.div`
   color: palevioletred;
 `;
 
-const Slider = styled.div`
-  height: 30vh;
-  display: flex;
-  justify-content: center;
-  background: #FED7D7
-`;
+// const Slider = styled.div`
+//   height: 30vh;
+//   display: flex;
+//   justify-content: center;
+//   background: #FED7D7
+// `;
+
+const styles = {
+  view_port: {
+    position: 'absolute',
+    top: '80%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '750px',
+    height: '200px',
+    //background: 'rgba(255, 255, 255, 0.5)',
+    overflow: 'hidden'
+  },
+  card_container: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: 'fit-content',
+  },
+};
 
 const products = [
-  { name: 'pic1', url: '../images/test_related_1.jpg' },
-  { name: 'pic2', url: '../images/test_related_2.jpg' },
-  { name: 'pic3', url: '../images/test_related_3.jpg' },
-  { name: 'pic4', url: '../images/test_related_1.jpg' },
-  { name: 'pic5', url: '../images/test_related_2.jpg' }];
+  { name: 'pic1', category: 'dress', price: '80', url: '../images/test_related_1.jpg' },
+  { name: 'pic2', category: 'shirt', price: '100', url: '../images/test_related_2.jpg' },
+  { name: 'pic3', category: 'skirt', price: '200', url: '../images/test_related_3.jpg' },
+  { name: 'pic4', category: 'pants', price: '999', url: '../images/test_related_1.jpg' },
+  { name: 'pic5', category: 'dress', price: '88', url: '../images/test_related_2.jpg' },
+  { name: 'pic6', category: 'glasses', price: '22', url: '../images/test_related_3.jpg' },
+  { name: 'pic7', category: 'dress', price: '33', url: '../images/test_related_2.jpg' },
+  { name: 'pic8', category: 'whatever', price: '490', url: '../images/test_related_1.jpg' },
+  { name: 'pic9', category: 'idontknow', price: '500', url: '../images/test_related_2.jpg' },
+  { name: 'pic10', category: 'lastone', price: '102', url: '../images/test_related_3.jpg' }];
 
-const RelatedProductsList = (props) => {
+const RelatedProductsList = () => {
   const [current, setCurrent] = useState(0);
-  const len = products.length;
+  const cardContainer = React.useRef();
 
-  const prevSilde = (e) => {
-    setCurrent(current === 0 ? len - 1 : current - 1);
+  useEffect(() => {
+    cardContainer.current.style.transitionDuration = '0.5s';
+    cardContainer.current.style.transform = `translate(-${200 * current}px)`;
+  }, [current]);
+
+  const prevSilde = () => {
+    if (current > 0) {
+      setCurrent(current - 1);
+    } else {
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
   };
 
-  const nextSlide = (e) => {
-    setCurrent(current === len - 1 ? 0 : current + 1);
+  const nextSlide = () => {
+    if (current < cardContainer.current.children.length - 1) {
+      setCurrent(current + 1);
+    } else {
+      return;
+    }
   };
 
   return (
     <div className="list_container">
       <Title>RELATED PRODUCTS</Title>
-      <button name="prev_btn" type="button" onClick={prevSilde}>Previous</button>
-      <Slider>
-        {products.map((product, index) => (
-          <div key={index}>
-            {index === current && (<ProductCard key={product.name} product={product} />)}
+      {current === 0 ? null : <button name="prev_btn" type="button" onClick={prevSilde}>Previous</button>}
+      {current === 6 ? null : <button name="next_btn" type="button" onClick={nextSlide}>Next</button>}
+
+      <div>
+        <div className="view-port" style={styles.view_port}>
+          <div ref={cardContainer} className="card-container" style={styles.card_container}>
+            {products.map((product, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <div key={index}>
+                <ProductCard key={product.name} product={product} />
+              </div>
+            ))}
           </div>
-        ))}
-      </Slider>
-      <button name="next_btn" type="button" onClick={nextSlide}>Next</button>
+        </div>
+      </div>
     </div>
+
   );
 };
 
