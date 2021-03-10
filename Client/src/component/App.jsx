fce9d78a3b7c0d1cc84814d71913605adbcf6501
sam <sparks>
2021-03-09 16:08:35 -0800

    styling in reviewList

--- Client/src/component/App.jsx
+++ Client/src/component/App.jsx
@@ -14,7 +14,7 @@ const AppBackground = styled.div`
 
 const AppBody = styled.div`
   display: grid;
-  grid-template-rows: 25% 23% 25% 27%;
+  grid-template-autp: 25% 23% 25% 27%;
   grid-template-columns: auto;
   box-shadow: 10px 50px 100px 0 rgba(0,0,0,0.1);
   background: rgba(203, 216, 225, 1);
