5c62b269e502fab93812e58c2006a806473bae0f
gabinochacon8 <gabino.chacon8@gmail.com>
2021-03-11 23:57:36 -0800

    clean up and styling adjust

--- Client/src/component/OverviewThumbnails.jsx
+++ Client/src/component/OverviewThumbnails.jsx
@@ -7,20 +7,17 @@ const styles = {
   thumbnails: {
     width: '50px',
     height: '60px',
-    border: '1px solid black',
-    borderSizing: 'border-box',
+    border: '3px solid #344B5B',
     boxSizing: 'border-box',
     fontSize: '2.5em',
+    borderRadius: '3px'
   }
 };
 
-const Thumbnails = ({ idx, image, onThumbnailClick }) => {
-  const randomVar = 'to make eslint happy';
-  return (
-    <div style={styles.thumbnails}>
-      <img src={image} onClick={() => onThumbnailClick(idx)} alt="" height="60px" width="50px" />
-    </div>
-  );
-};
+const Thumbnails = ({ idx, image, onThumbnailClick }) => (
+  <div style={styles.thumbnails}>
+    <img src={image} onClick={() => onThumbnailClick(idx)} alt="" height="60px" width="50px" />
+  </div>
+);
 
 export default Thumbnails;
